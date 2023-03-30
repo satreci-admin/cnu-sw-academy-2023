5d24a4bf93252f75014d95c803630c00dba87b9a
ham3798 <5023798@naver.com>
2023-03-30 23:16:00 +0900

    build: robot entity 추가

--- src/main/java/com/cnu/simple/robot/Robot.java
+++ src/main/java/com/cnu/simple/robot/Robot.java
@@ -1,5 +1,6 @@
 package com.cnu.simple.robot;
 
+import com.cnu.simple.work.WorkSpecification;
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -13,4 +14,26 @@ public class Robot {
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "id", nullable = false, unique = true, columnDefinition = "BINARY(16)")
     private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "SSH_Id")
+    private String sshId;
+
+    @Column(name = "SSH_PW")
+    private String sshPw;
+
+    @Column(name = "port")
+    private int port;
+
+    @ManyToOne
+    @JoinColumn(name = "work_spec_id", referencedColumnName = "id")
+    private WorkSpecification workSpecification;
+
+    @Column(name = "IP")
+    private String ip;
+
+    @Column(name = "memo")
+    private String memo;
 }
