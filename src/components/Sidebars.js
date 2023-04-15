8789c80e57f82701ca1d7c4110a8ea77dea619b8
wjdtjdgns <person3275@gmail.com>
2023-04-15 13:59:51 +0900

    feat: Sidebar 구현

---
+++ src/components/Sidebars.js
@@ -0,0 +1,62 @@
+import React, { useState, memo } from "react";
+import { Link } from "react-router-dom";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import styled from "styled-components";
+
+const SideBarWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  background-color: #f9f9f9;
+  height: 100%;
+  width: 230px;
+  padding: 10px 20px;
+  z-index: 99;
+`;
+
+const SideBarHeader = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const StyledLink = styled(MenuItem)`
+  text-decoration: none;
+  font-weight: ${({ active }) => (active === "true" ? "800" : "600")};
+  color: ${({ active }) =>
+    active === "true" ? "black" : "rgba(0, 0, 0, 0.5)"};
+  background-color: ${({ active }) =>
+    active === "true" ? "#f2f2f2" : "#f9f9f9"};
+  border-radius: 5px;
+`;
+
+const MemoStyledLink = memo(StyledLink);
+
+function Sidebars() {
+  const menus = [
+    { name: "로봇 관리", path: "/robot" },
+    { name: "작업명세서 관리", path: "/work-specification" },
+    { name: "사용자 관리", path: "/member" },
+  ];
+
+  const [click, setClick] = useState("/robot");
+
+  return (
+    <SideBarWrapper>
+      <Sidebar>
+        <SideBarHeader>Simple RPA</SideBarHeader>
+        <Menu>
+          {menus.map((menu, idx) => (
+            <MemoStyledLink
+              component={<Link to={menu.path} />}
+              key={idx}
+              active={menu.path === click ? "true" : "false"}
+              onClick={() => setClick(menu.path)}
+            >
+              {menu.name}
+            </MemoStyledLink>
+          ))}
+        </Menu>
+      </Sidebar>
+    </SideBarWrapper>
+  );
+}
+
+export default Sidebars;
