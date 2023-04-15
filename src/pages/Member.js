c9ab3b335ae5a7b21933e89d5bb1349b4fe91a73
wjdtjdgns <person3275@gmail.com>
2023-04-15 15:32:16 +0900

    feat: Header 구현

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,7 +1,14 @@
 import React from "react";
 import MainContentWrapper from "../components/MainContentWrapper";
+import Header from "../components/Header";
+
 function Member() {
-  return <MainContentWrapper>member</MainContentWrapper>;
+  return (
+    <MainContentWrapper>
+      <Header mainHeader={"사용자 관리"} subHeader={"User Management"}></Header>
+      member
+    </MainContentWrapper>
+  );
 }
 
 export default Member;
--- src/pages/Robot.js
+++ src/pages/Robot.js
@@ -1,8 +1,17 @@
 import React from "react";
 import MainContentWrapper from "../components/MainContentWrapper";
+import Header from "../components/Header";
 
 function Robot() {
-  return <MainContentWrapper>Robot</MainContentWrapper>;
+  return (
+    <MainContentWrapper>
+      <Header
+        mainHeader={"로봇 관리"}
+        subHeader={"Supporting robot management"}
+      ></Header>
+      robot
+    </MainContentWrapper>
+  );
 }
 
 export default Robot;
