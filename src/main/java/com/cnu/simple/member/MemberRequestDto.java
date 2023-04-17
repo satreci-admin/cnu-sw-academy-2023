8939930b5f4fc557b785e5741d6765e034e06a23
beautiflow <1114ny@naver.com>
2023-04-18 08:56:42 +0900

    feat: MemberRepository, MemberListResponseDto, MemberResponseDto, MemberRequestDto 클래스 추가

---
+++ src/main/java/com/cnu/simple/member/MemberRequestDto.java
@@ -0,0 +1,38 @@
+package com.cnu.simple.member;
+
+import com.cnu.simple.robot.Robot;
+import com.cnu.simple.work.WorkSpecification;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberRequestDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<WorkSpecification> workSpecifications;
+
+    @Builder
+    public MemberRequestDto(Long id, String name, String email, String password, List<WorkSpecification> workSpecifications) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.workSpecifications = workSpecifications;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .id(this.id)
+                .name(this.name)
+                .email(this.email)
+                .password(this.password)
+                .workSpecification(this.workSpecifications)
+                .build();
+    }
+
+}
