5054fa7143c9fddae4e80f02764393176c5c7633
beautiflow <1114ny@naver.com>
2023-04-18 08:53:07 +0900

    feat: Member 엔티티 수정

--- src/main/java/com/cnu/simple/member/Member.java
+++ src/main/java/com/cnu/simple/member/Member.java
@@ -2,34 +2,42 @@ package com.cnu.simple.member;
 
 import com.cnu.simple.work.WorkSpecification;
 import jakarta.persistence.*;
-import org.hibernate.annotations.GenericGenerator;
+import lombok.*;
 
 import java.util.List;
-import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
-@Table(name = "members")
+@Table(name = "member")
+@NoArgsConstructor
 public class Member {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(name = "id", nullable = false, unique = true, columnDefinition = "BINARY(16)")
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false, unique = true)
+    private static Long id;
 
-    @Column(nullable = false, unique = true, columnDefinition = "TEXT", length = 45)
-    private String name;
+    @Column(name = "name", nullable = false, unique = true, columnDefinition = "VARCHAR(45)", length = 45)
+    private static String name;
 
-    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(45)", length = 45)
-    private String email;
+    @Column(name = "email", nullable = false, unique = true, columnDefinition = "VARCHAR(45)", length = 45)
+    private static String email;
 
-    @Column(nullable = false, columnDefinition = "")
-    private String password;
+    @Column(name = "password", nullable = false, columnDefinition = "")
+    private static String password;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private List<WorkSpecification> workSpecification;
+    private static List<WorkSpecification> workSpecification;
 
-
+    @Builder
+    public Member(Long id, String name, String email, String password, List<WorkSpecification> workSpecification) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.workSpecification = workSpecification;
+    }
 }
 
 
